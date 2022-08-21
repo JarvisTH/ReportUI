99cd5cbfb4bb1bc25e6169f9cf64b844e011fe99
JarvisTH <1373661072@qq.com>
2022-08-21 23:29:59 +0800

    提交

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var $duration = 1000;
+$(document).ready(function(){
+    // - ACCOUNT DROPDOWN
+    $('.ui.admindropdown').dropdown({
+        transition: 'drop',
+        on : 'click',
+        duration  : 500
+    });
+    // - SHOW & HIDE SIDEBAR
+    $("#showmobiletabletsidebar").click(function(){
+        $('.mobiletabletsidebar.animate .menu').transition({
+            animation : 'swing right',
+            duration  : $duration
+        })
+        ;
+        $('#mobiletabletsidebar').removeClass('hidden');
+    });
+    // - SHOW & HIDE SIDEBAR
+    $("#showmobiletabletsidebar").click(function(){
+        $('.mobiletabletsidebar.animate .menu').transition({
+            animation : 'swing right',
+            duration  : $duration
+        })
+        ;
+        $('#mobiletabletsidebar').removeClass('hidden');
+    });
+    $("#hidemobiletabletsidebar").click(function(){
+        $('.mobiletabletsidebar.animate .menu')
+            .transition({
+                animation : 'fade',
+                duration  : $duration
+            });
+    });
+    $(".ui.accordion").accordion({
+        exclusive: false
+    });
+});
